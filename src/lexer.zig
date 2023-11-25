fe689785c8abc6a1ae1d38e8c4e169e208bd8af2
Guillaume <guillaume.thouvenin@vates.tech>
2023-11-25 12:47:18 +0100

    lexer can now tokenize simple input

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,8 +1,86 @@
 const token = @import("token.zig");
+const std = @import("std");
 
-const Lexer = struct {};
+const Lexer = struct {
+    input: []u8,
+    position: usize, // current position in input (points to current char)
+    readPosition: usize, // current reading position in input (after current char)
+    ch: u8, // current char under examination (only ASCII is supported)
+    allocator: std.mem.Allocator,
 
-test "lexer" {
+    /// Read the next character and advance the position. If the
+    /// end of input is reached then read character is set to 0.
+    fn readChar(self: *Lexer) void {
+        if (self.readPosition >= self.input.len) {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.readPosition];
+            self.position = self.readPosition;
+            self.readPosition += 1;
+        }
+    }
+
+    pub fn new(allocator: std.mem.Allocator, input: []const u8) !Lexer {
+        var l = Lexer{
+            .input = try allocator.dupe(u8, input),
+            .position = 0,
+            .readPosition = 0,
+            .ch = 0,
+            .allocator = allocator,
+        };
+        // before returning l update it by reading the first
+        // character
+        l.readChar();
+        return l;
+    }
+
+    pub fn free(self: *Lexer) void {
+        self.allocator.free(self.input);
+    }
+
+    pub fn nextToken(self: *Lexer) token.Token {
+        const slice: []u8 = self.input[self.position .. self.position + 1];
+        const tok = switch (self.ch) {
+            '=' => token.Token.new(.assign, slice),
+            ';' => token.Token.new(.semicolon, slice),
+            '(' => token.Token.new(.lparen, slice),
+            ')' => token.Token.new(.rparen, slice),
+            ',' => token.Token.new(.comma, slice),
+            '+' => token.Token.new(.plus, slice),
+            '{' => token.Token.new(.lbrace, slice),
+            '}' => token.Token.new(.rbrace, slice),
+            else => token.Token.new(.eof, ""),
+        };
+        // Before returning the token let's advance pointers into input.
+        self.readChar();
+
+        return tok;
+    }
+};
+
+test "simple tokens" {
     const input = "=+(){},;";
-    _ = input;
+    var l = try Lexer.new(std.testing.allocator, input);
+    defer l.free();
+
+    var t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.assign);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.plus);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.lparen);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.rparen);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.lbrace);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.rbrace);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.comma);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.semicolon);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.eof);
+    t = l.nextToken();
+    std.debug.assert(t.type == token.TokenType.eof);
 }
--- src/token.zig
+++ src/token.zig
@@ -1,32 +1,67 @@
-const Token = struct {
-    type: TokenType,
-    literal: []const u8,
-};
-
-const TokenType = enum([]const u8) {
+pub const TokenType = enum {
     // Special types
-    illegal = "ILLEGAL",
-    eof = "EOF",
+    illegal,
+    eof,
 
     // Identifiers and litterals
-    ident = "IDENT", // add, foobar, x, y, ...
-    int = "INT", // 1234
+    ident, // add, foobar, x, y, ...
+    int, // 1234
 
     // Operators
-    assign = "=",
-    plus = "+",
+    assign,
+    plus,
 
     // Delimiters
-    comma = ",",
-    semicolon = ";",
-    lparen = "(",
-    rparen = ")",
-    lbrace = "{",
-    rbrace = "}",
+    comma,
+    semicolon,
+    lparen,
+    rparen,
+    lbrace,
+    rbrace,
 
     // Keywords
-    function = "FUNCTION",
-    let = "LET",
+    function,
+    let,
+
+    pub fn string_of_tokenType(tok: TokenType) []const u8 {
+        return switch (tok) {
+            // Special types
+            .illegal => "ILLEGAL",
+            .eof => "EOF",
+
+            // Identifiers and litterals
+            .ident => "IDENT", // add, foobar, x, y, ...
+            .int => "INT", // 1234
+
+            // Operators
+            .assign => "=",
+            .plus => "+",
+
+            // Delimiters
+            .comma => ",",
+            .semicolon => ";",
+            .lparen => "(",
+            .rparen => ")",
+            .lbrace => "{",
+            .rbrace => "}",
+
+            // Keywords
+            .function => "FUNCTION",
+            .let => "LET",
+        };
+    }
+};
+
+pub const Token = struct {
+    type: TokenType,
+    literal: []u8, // slice
+
+    pub fn new(ttype: TokenType, literal: []u8) Token {
+        return .{
+            .type = ttype,
+            .literal = literal,
+        };
+    }
 };
 
 test "token" {}
