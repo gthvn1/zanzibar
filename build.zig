813859f98dd97f8fbadf9525348cc0944bf85f40
Guillaume <guillaume.thouvenin@vates.tech>
2024-08-18 14:45:17 +0200

    create a unit tests step

--- build.zig
+++ build.zig
@@ -9,8 +9,18 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
+    // Add run cmd
     const run_cmd = b.addRunArtifact(exe);
-
+    // And add its corresponding step
     const run_step = b.step("run", "Run zanzibar repl");
     run_step.dependOn(&run_cmd.step);
+
+    // Create a step for running unit tests
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+    });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,7 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("Hello, Sailor!\n", .{});
 }
+
+test "always succeeds" {
+    try std.testing.expect(true);
+}
