a856d0d1303f43e728792b8fd9e27cf218d70985
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-29 13:49:05 +0100

    Restart project from scratch

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,27 @@
 const std = @import("std");
+const zanzibar = @import("zanzibar");
 
-pub fn main() void {
-    std.debug.print("Hello, Sailor!\n", .{});
+pub fn main() !void {
+    // Prints to stderr, ignoring potential errors.
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try zanzibar.bufferedPrint();
 }
 
-test "always succeeds" {
-    try std.testing.expect(true);
+test "simple test" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(i32) = .empty;
+    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(gpa, 42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "fuzz example" {
+    const Context = struct {
+        fn testOne(context: @This(), input: []const u8) anyerror!void {
+            _ = context;
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        }
+    };
+    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
