7039ac204a9f94e8e0bc465e9cc350f089c5971f
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-30 09:54:28 +0100

    Use stdout instead of log

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const zanzibar = @import("zanzibar");
 
 pub fn main() void {
-    var log_buffer: [256]u8 = undefined;
-    var log_writer = std.fs.File.stdout().writer(&log_buffer);
-    const log = &log_writer.interface;
+    var stdout_buffer: [256]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
     var stdin_buffer: [256]u8 = undefined;
     var stdin_reader = std.fs.File.stdout().reader(&stdin_buffer);
     const stdin = &stdin_reader.interface;
 
-    zanzibar.startRepl(stdin, log);
+    zanzibar.startRepl(stdin, stdout);
 }
