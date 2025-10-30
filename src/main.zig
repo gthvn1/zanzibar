b88a28b9b9992fe7539466dfd561c332d1fe53b8
Guillaume <guillaume.thouvenin@vates.tech>
2025-10-30 09:52:24 +0100

    Move repl in its own module

--- src/main.zig
+++ src/main.zig
@@ -10,5 +10,5 @@ pub fn main() void {
     var stdin_reader = std.fs.File.stdout().reader(&stdin_buffer);
     const stdin = &stdin_reader.interface;
 
-    zanzibar.startRepl(stdin, log) catch std.debug.print("Failed to start the REPL\n", .{});
+    zanzibar.startRepl(stdin, log);
 }
---
+++ src/repl.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const Lexer = @import("lexer.zig").Lexer;
+
+pub fn start(reader: *std.Io.Reader, writer: *std.Io.Writer) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+
+    var lexer = Lexer.init(gpa.allocator());
+    defer lexer.deinit();
+
+    const menu_str =
+        \\Welcome to Monkey Islang !!!
+        \\Feel free to type Monkey code or 'quit'
+    ;
+
+    const bye_str =
+        \\May your trip be as enjoyable as finding extra
+        \\bananas at the bottom of the bag!
+    ;
+
+    try writer.print("{s}\n", .{menu_str});
+
+    loop: while (true) {
+        try writer.writeAll(">> ");
+        try writer.flush();
+
+        const line = reader.takeDelimiterExclusive('\n') catch |err| switch (err) {
+            error.EndOfStream => {
+                // reached end
+                // the normal case
+                try writer.print("\n{s}\n", .{bye_str});
+                try writer.flush();
+                return;
+            },
+            error.StreamTooLong => {
+                try writer.writeAll("ERROR: the line was longer than the internal buffer\n");
+                continue :loop;
+            },
+            error.ReadFailed => {
+                try writer.writeAll("ERROR: the read failed\n");
+                continue :loop;
+            },
+        };
+
+        // Should we quit?
+        if (line.len == "quit".len) {
+            var buf: [4]u8 = undefined;
+            const quit = std.ascii.lowerString(&buf, line);
+
+            if (std.mem.eql(u8, "quit", quit)) {
+                try writer.print("\n{s}\n", .{bye_str});
+                try writer.flush();
+                return;
+            }
+        }
+
+        // Consume the '\n' before continuing
+        reader.toss(1);
+
+        try writer.print("You typed: <{s}>\n", .{line});
+        try writer.flush();
+
+        lexer.transform(line);
+    }
+}
